01e93bd63bd6f274f6fb37acdca80e8c536f3ad8
AMMAR AL HADDAD <91353209+ammaralhaddad@users.noreply.github.com>
2021-10-25 18:22:24 -0400

    Create Template Literals.js

---
+++ Template Literals.js
@@ -0,0 +1,14 @@
+const assertEqual = function(a,b){
+if (a === b){
+
+console.log(`Assertion Passed:${a}===${b}`)
+
+
+}else{
+
+  console.log(`Assertion Failed: ${a} !== ${b}`)
+}
+
+
+}
+assertEqual(1,1)

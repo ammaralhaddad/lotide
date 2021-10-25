0fabad6bed1e906337b3d2127a9eb965ef5097ca
AMMAR AL HADDAD <91353209+ammaralhaddad@users.noreply.github.com>
2021-10-25 18:36:11 -0400

    Create head.js

---
+++ head.js
@@ -0,0 +1,23 @@
+const assertEqual = function(a,b){
+if (a === b){
+
+console.log(`Assertion Passed:${a}===${b}`)
+
+
+}else{
+
+  console.log(`Assertion Failed: ${a} !== ${b}`)
+}
+
+
+}
+function head(array) {
+  if (array === []) {
+    return undefined;
+  } else {
+    return array[0];
+  }
+}
+
+assertEqual(head([5, 8, 0]), 8);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "labs");

3e7d1932d1815eeb043e2e709c0f9554cc43f916
AMMAR AL HADDAD <91353209+ammaralhaddad@users.noreply.github.com>
2021-10-25 17:44:00 -0400

    Create assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(a,b){
+if (a === b){
+
+console.log('Assertion Passed: 1 === 1')
+
+
+}else{
+
+  console.log("Assertion Failed: Lighthouse Labs !== Bootcamp")
+}
+
+
+}
+assertEqual1,1)

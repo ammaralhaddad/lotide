9170e272bf85f9d5e6b4b20d0726929aeb7cfb22
AMMAR AL HADDAD <91353209+ammaralhaddad@users.noreply.github.com>
2021-10-25 19:25:32 -0400

    Update head.js

--- head.js
+++ head.js
@@ -1,23 +1,26 @@
-const assertEqual = function(a,b){
-if (a === b){
-
-console.log(`Assertion Passed:${a}===${b}`)
-
-
-}else{
-
-  console.log(`Assertion Failed: ${a} !== ${b}`)
-}
-
-
-}
-function head(array) {
-  if (array === []) {
-    return undefined;
-  } else {
-    return array[0];
+cconst assertEqual = function(a,b){
+  if (a === b){
+  
+  console.log(`Assertion Passed:${a}===${b}`)
+  
+  
+  }else{
+  
+    console.log(`Assertion Failed: ${a} !== ${b}`)
   }
-}
+  
+  
+  }
+  const head =function(array) {
+    if (array === []) {
+      return undefined;
+    } else {
+      return array[0];
+    }
+  }
+  
+ 
+  
 
 assertEqual(head([5, 8, 0]), 8);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "labs");

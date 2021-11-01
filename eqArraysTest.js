e07540bd08947c3c161e7addac12a02600515045
AMMAR AL HADDAD <91353209+ammaralhaddad@users.noreply.github.com>
2021-11-01 15:49:17 -0400

    Create eqArraysTest.js

---
+++ eqArraysTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+const eqArraysTest = require('../eqarrays');
+
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should fail
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should fail 
